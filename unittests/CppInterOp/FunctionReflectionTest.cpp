22ff87e53f3cc9aff3e6ef619104e6c3ff081cab
kr-2003 <kumar.kr.abhinav@gmail.com>
2025-04-14 12:34:46 +0530

    g

--- unittests/CppInterOp/FunctionReflectionTest.cpp
+++ unittests/CppInterOp/FunctionReflectionTest.cpp
@@ -637,7 +637,8 @@ TEST(FunctionReflectionTest, ExistsFunctionTemplate) {
 TEST(FunctionReflectionTest, InstantiateTemplateFunctionFromString) {
   if (llvm::sys::RunningOnValgrind())
     GTEST_SKIP() << "XFAIL due to Valgrind report";
-  Cpp::CreateInterpreter();
+  std::vector<const char*> interpreter_args = { "-include", "new" };
+  Cpp::CreateInterpreter(interpreter_args);
   std::string code = R"(#include <memory>)";
   Interp->process(code);
   const char* str = "std::make_unique<int,int>";
